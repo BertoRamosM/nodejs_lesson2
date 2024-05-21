60a8607bcddd0cac44957abde9230fe99c9f4e8d
ARamos <albertramos902@gmail.com>
2024-05-21 10:21:52 +0100

    refractoring header and status code

--- http.js
+++ http.js
@@ -5,21 +5,18 @@ const desiredPort = process.env.PORT ?? 1234;
 
 const processRequest = (req, res) => {
   // in case all the headers are the same we can just declare them here, and all of the response will share it instead of writting one in each
- /*  res.setHeader("Content-Type", "text/html; charset=utf-8"); */
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  // or text/plain, or application/json, or image etc etc etc
 
   if (req.url === "/") {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-    // or text/html, or application/json, or image etc etc etc
-    res.end("Welcome to my main page")
-  } else if (req.url === '/contact') {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    //default code = 200 its actually the default
+ /*    res.statusCode = 200; */
+    res.end("<h1>Welcome to my main page</h1>");
+  } else if (req.url === "/contact") {
     res.end("<h1>Contact page</h1>");
   } else {
-    res.statusCode = 404
-     res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.end("404 page not found")
+    res.statusCode = 404;
+    res.end("<h1>404 page not found</h1>");
   }
 }
 
