87bdfcdb61854fb435e35508a703603dc6fed27f
ARamos <albertramos902@gmail.com>
2024-05-21 11:42:27 +0100

    using method get to get a pokemon info

--- 2.routing.js
+++ 2.routing.js
@@ -1,15 +1,32 @@
-const http = require('node:http')
+const http = require("node:http");
+
+//commonJS(modules) we can import a json a saco
+const ditoJSON = require('./pokemon.json')
 
 const processRequest = (req, res) => {
+  const { method, url } = req;
 
-}
+  switch (method) {
+    case 'GET':
+      switch (url) {
+        case '/pokemon/ditto':
+          res.setHeader('Content-Type', 'application/json; charset=utf-8')
+          return res.end(JSON.stringify(ditoJSON))  
+        default:
+          res.statusCode = 404
+           res.setHeader("Content-Type", "text/html; charset=utf-8");
+          return res.end('<h1>404Not found</h1>')
+      }
 
-const server = http.createServer(processRequest)
+      case 'POST':
+  }
+};
+
+const server = http.createServer(processRequest);
 
 server.listen(1234, () => {
-  console.log(`server listening on port http://localhost:1234`)
-})
-
+  console.log(`server listening on port http://localhost:1234`);
+});
 
 //list of methods
 /* 
