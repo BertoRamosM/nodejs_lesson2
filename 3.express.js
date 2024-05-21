736691d7ef0fe4614f6022dc3312c4bdb0989d33
ARamos <albertramos902@gmail.com>
2024-05-21 15:39:49 +0100

    added 404 with express

--- 3.express.js
+++ 3.express.js
@@ -1,15 +1,19 @@
 const express = require("express")
 const fs = require("node:fs")
+const ditto = require("./pokemon/ditto.json")
 
 const app = express()
 
+//we delete the watermark of express in our request
+app.disable("x-powered-by")
+
 const PORT = process.env.PORT ?? 1234
 
-app.get("/", (req, res) => {
-  res.status(200).send('<h1>My main page in express!</h1>')
-  //we could also send a json like this
-  /* 
-  res.json({message: "Hello world!"})
+app.get("/pokemon/ditto", (req, res) => {
+  //we can send a json easily:
+  res.json(ditto);
+  /* we could also send html like this:
+   res.status(200).send('<h1>My main page in express!</h1>')
   */
 })
 
@@ -37,6 +41,10 @@ app.get("/pink", (req, res) => {
   })
 });
 
+// the 404 has to be always the last route to be declared, without path
+app.use((req, res) => {
+  res.status(400).send('<h1>404 page not found</h1>')
+})
 
 app.listen(PORT, () => {
   console.log(`Listing server in port ${PORT}`)
