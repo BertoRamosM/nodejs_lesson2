e1bb0251b18aa3f32ccb88bf7a1e2cfe38e95b47
ARamos <albertramos902@gmail.com>
2024-05-21 15:29:34 +0100

    created post method with express

--- 3.express.js
+++ 3.express.js
@@ -7,6 +7,22 @@ const PORT = process.env.PORT ?? 1234
 
 app.get("/", (req, res) => {
   res.status(200).send('<h1>My main page in express!</h1>')
+  //we could also send a json like this
+  /* 
+  res.json({message: "Hello world!"})
+  */
+})
+
+app.post('/pokemon', (req, res) => {
+    let body = ``
+  req.on("data", chunk => {
+      body += chunk.toString()
+  })
+  req.on("end", () => {
+    const data = JSON.parse(body)
+    data.timeStamp = Date.now()
+    res.status(201).json(data)
+  })
 })
 
 
